822066b81788745efad8229ab777de71df8dc6db
mintanwei <mintanwei@126.com>
2019-09-19 10:05:09 +0800

    Update parameter_define.py

--- parameter_define.py
+++ parameter_define.py
@@ -9,7 +9,7 @@ dirpath = 'RetargetMeAll/'  # test path
 img_ext = '.png'
 outpath = './test_result'  # test output path
 
-grid_size = 16
+grid_size = 8
 grid_min = 3.0  #16*0.2
 
 VGG_MEAN = [103.939, 116.779, 123.68]
@@ -17,7 +17,7 @@ SEED = 664  # Set to None for random seed.
 batch_size = 4
 img_height = 240
 img_width = 320
-learning_rate_ger = 1e-3
+learning_rate_ger = 5e-4
 device = '/gpu:0'
 
 is_adam = True
@@ -35,4 +35,4 @@ if not os.path.exists(log_dir):
 if not os.path.exists(outpath):
     os.makedirs(outpath)
 
-num_epochs = 30
+num_epochs = 10
